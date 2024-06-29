bb5dde8c4db8fff4e0780c0e8f64aa940d5245a3
Priyom1 <soikatmirror23@gmail.com>
2024-06-30 01:04:05 +0600

    frontend

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Our E-commerce Site</h1>
+      <p>Explore our wide range of products and enjoy your shopping experience!</p>
+    </div>
+  );
+};
+
+export default Home;
